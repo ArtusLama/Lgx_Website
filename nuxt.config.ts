c8ceb893c950b9b2d53fe3182b8d88f1010339be
Artus <artuslama@gmail.com>
2026-02-28 19:00:21 +0100

    🔧 Setup ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: ["@nuxt/eslint", "@nuxtjs/stylelint-module"],
+
+    eslint: {
+        config: {
+            stylistic: true,
+        },
+    },
 })
---
+++ stylelint.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-recommended-vue",
+        "stylelint-config-tailwindcss",
+    ],
+    plugins: ["@stylistic/stylelint-plugin"],
+    rules: {
+        "@stylistic/indentation": 4,
+        "@stylistic/declaration-colon-space-after": "always",
+        "hue-degree-notation": null,
+        "lightness-notation": null,
+        "selector-class-pattern": null,
+    },
+}
