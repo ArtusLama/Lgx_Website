970b652fcf8d0721c0a6d4cb7cb2c12c29bdd442
Artus <artuslama@gmail.com>
2026-03-11 18:03:40 +0100

    ⚰️ Delete old shop cards

--- app/components/section/order/old/select/SidebarWrapper.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <UiRadioGroup>
-            <UiAccordion type="multiple" collapsible>
-                <slot />
-            </UiAccordion>
-        </UiRadioGroup>
-    </div>
-</template>
